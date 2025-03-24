bfc76f9068deb4139039d230170503b2515984ab
Pankaj Yadav <157686378+pankajydv07@users.noreply.github.com>
2025-03-25 01:03:25 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 
 
 // client/src/utils/api.js
-const API_URL = process.env.NODE_ENV === 'production' 
-  ? '/api' 
-  : 'http://localhost:5000/api';
-
+const API_URL = '/api'
 
 // User API calls
 export const loginUser = async (loginData) => {
